67f46814a731394888241f9b111249f0185bc35b
Domenico D'antò <domenic-90@hotmail.it>
2021-11-09 12:48:12 +0100

    api key update

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
 
   //metodo per lettura e chiamata api in formato Json da weatherstack.com
   getWeather(location: any) {
-    return this.http.get('http://api.weatherstack.com/current?access_key=93c5565c7b4b50660bd820dc073ed0b9&query=' + location);
+    return this.http.get('http://api.weatherstack.com/current?access_key=1c4cda06b0437df1d65c99461ae56dc7&query=' + location);
   }
 }
 
@@ -18,5 +18,6 @@ export class ApiService {
 /* Chiavi Api di riserva
 5bcaf638554927c750263460034bfe42
 bd9f57d7283a2dc4578b857b1f55b854
-98082d4ce2b96a002d8c607c986354ef */
+98082d4ce2b96a002d8c607c986354ef
+93c5565c7b4b50660bd820dc073ed0b9*/
 
